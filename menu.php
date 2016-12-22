23c5e9f994be630f26a532b829d897b945bdb61d
IrvanKurniansyah7979 <irvan7979@students.amikom.ac.id>
2016-12-22 20:06:20 +0700

    menu
    menampilkan 3 menu pertama

---
+++ menu.php
@@ -0,0 +1,8 @@
+<div class="menu">
+                    <ul>
+                    <li><a class="current" href="index.php">Home</a></li>
+                    <li><a href="?act=buku">Daftar Buku</a>
+                    <li><a href="?act=siswa">Daftar Siswa</a>
+                    </li>
+                    </ul>
+                    </div>
