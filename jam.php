84124d01a35bf05b85b0b9bcee181a71c7cf452b
MFadhliA <muhammad7967@students.amikom.ac.id>
2016-12-22 22:18:10 +0700

    fix jam

---
+++ jam.php
@@ -0,0 +1,15 @@
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+
+         
+            <script type="text/javascript">
+AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0','width','170','height','68','title','jam','src','free-flash-clock-152-2','quality','high','pluginspage','http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash','movie','free-flash-clock-152-2' ); //end AC code
+            </script>
+            <noscript>
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,15,0" width="170" height="68" title="jam">
+              <param name="movie" value="free-flash-clock-152-2.swf" />
+              <param name="quality" value="high" />
+              <embed src="free-flash-clock-152-2.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="202" height="68"></embed>
+            </object>
+            </noscript>
+          </span>
+       
