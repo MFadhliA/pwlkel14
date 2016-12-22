37fab450cdac07c011ea8845a788c1630866cf8b
MFadhliA <muhammad7967@students.amikom.ac.id>
2016-12-22 22:20:39 +0700

    fix teks utama

---
+++ utama.php
@@ -0,0 +1,6 @@
+<h2>Perpustakaan Musaba</h2>
+
+<div align="center">
+<img src="images/logo.png"  alt=""/></div>
+<p>Selamat datang di perpustakaan MUSABA!!, Anda bisa memilih dan meminjam buku di sini dengan gratis!!
+    
